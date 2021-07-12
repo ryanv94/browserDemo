c96cb5877ea01cb1935b38bcb25c63f7f8cd114f
Ryan Veitch <37779995+ryanv94@users.noreply.github.com>
2021-07-12 13:52:06 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+userName = document.getElementById('nameInput').value;
+userEmail = document.getElementById('emailInput').value;
+
+function buttonClick() {
+
+    if (typeof newrelic == 'object') {
+        newrelic.addPageAction('Submit', {'username': userName, 'email': userEmail});
+        alert('Data sent to New Relic - try out: SELECT * FROM PageAction WHERE actionName = \'Submit\' SINCE TODAY')
+      }
+
+
+
+}
