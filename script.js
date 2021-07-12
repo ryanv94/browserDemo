fdee45ed3ffb84a8f7a2e8e051a8a7f1c1ca9ac2
Ryan Veitch <37779995+ryanv94@users.noreply.github.com>
2021-07-12 14:04:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var userEmail = document.getElementById('emailInput').value;
 function buttonClick() {
 
     if (typeof newrelic == 'object') {
-        newrelic.addPageAction('Submit', {'username': userName, 'email': userEmail});
+        newrelic.addPageAction('Submit', {username: userName, email: userEmail});
         alert('Data sent to New Relic - try out: SELECT * FROM PageAction WHERE actionName = \'Submit\' SINCE TODAY')
       }
 
